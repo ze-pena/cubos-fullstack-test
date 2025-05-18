6603646c11edad5646f21c88ff55cbbacf1e8a67
ze-pena <jose.pena.dev@gmail.com>
2025-05-18 14:16:56 -0300

    build: set up react-router-dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+import Routing from './routes'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hello world!</h1>
+    <Routing />
   </StrictMode>,
 )
